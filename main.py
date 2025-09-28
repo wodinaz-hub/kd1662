7a581000aee6096910a889e5e3baf14a08412463
wodinaz-hub <sanbalashov@gmail.com>
2025-09-28 19:45:38 +0300

    fixed double

--- main.py
+++ main.py
@@ -1,45 +1,36 @@
 import os
 import logging
-import sys # Import sys for StreamHandler
+import sys
 from dotenv import load_dotenv
 
-# Import bot_instance and run_bot directly from bot.commands
-from bot.commands import bot_instance, run_bot
+# ИМПОРТИРУЕМ ТОЛЬКО КЛАСС BotInstance
+from bot.commands import BotInstance
 
 print(f"STARTED: PID={os.getpid()}")
 
-# --- НАЧАЛО БЛОКА КОНФИГУРАЦИИ ЛОГИРОВАНИЯ (ОБНОВЛЕНО) ---
-# Создаем корневой логгер
+# --- НАЧАЛО БЛОКА КОНФИГУРАЦИИ ЛОГИРОВАНИЯ ---
 root_logger = logging.getLogger()
-root_logger.setLevel(logging.DEBUG) # Устанавливаем уровень DEBUG для корневого логгера
+root_logger.setLevel(logging.DEBUG)
 
-# Очищаем существующие обработчики, чтобы избежать дублирования
 for handler in root_logger.handlers[:]:
     root_logger.removeHandler(handler)
     handler.close()
 
-# Создаем хэндлер для вывода в консоль
 handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
 handler.setFormatter(formatter)
-handler.setLevel(logging.DEBUG) # Устанавливаем уровень DEBUG для хэндлера
+handler.setLevel(logging.DEBUG)
 root_logger.addHandler(handler)
 
-# Настройка логирования для Discord.py
-logging.getLogger('discord').setLevel(logging.INFO) # Discord.py можно оставить на INFO
-logging.getLogger('discord.http').setLevel(logging.INFO) # Http запросы Discord.py тоже на INFO
-
-# Настройка логирования для ваших модулей
-# Теперь логгер в calculator.py использует __name__, т.е. 'data_processing.calculator'
+logging.getLogger('discord').setLevel(logging.INFO)
+logging.getLogger('discord.http').setLevel(logging.INFO)
 logging.getLogger('data_processing.calculator').setLevel(logging.DEBUG)
-# Теперь логгер в chart_generator.py использует __name__, т.е. 'utils.chart_generator'
 logging.getLogger('utils.chart_generator').setLevel(logging.DEBUG)
-# Логгер в helpers.py также использует __name__, т.е. 'utils.helpers'
 logging.getLogger('utils.helpers').setLevel(logging.DEBUG)
 # --- КОНЕЦ БЛОКА КОНФИГУРАЦИИ ЛОГИРОВАНИЯ ---
 
 
-logger = logging.getLogger('main') # Этот логгер теперь будет использовать настроенный выше root_logger
+logger = logging.getLogger('main')
 
 # Load environment variables from .env file (e.g., DISCORD_BOT_TOKEN)
 load_dotenv()
@@ -52,7 +43,11 @@ if BOT_TOKEN is None:
 else:
     try:
         logger.info("Starting Discord bot...")
-        run_bot(BOT_TOKEN)
+
+        # --- ИСПРАВЛЕНИЕ: Создаем ОДИН экземпляр класса BotInstance и запускаем его ---
+        bot_instance = BotInstance()
+        bot_instance.bot.run(BOT_TOKEN)
+        # --- КОНЕЦ ИСПРАВЛЕНИЯ ---
+
     except Exception as e:
         logger.exception("An error occurred while running the bot:")
-
